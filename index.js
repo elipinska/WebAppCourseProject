a6877f3c499f102658b34c732b2015a14d6e0301
elipinska <ewa.lipinska@hotmail.com>
2017-10-09 19:31:28 +0100

    add map

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("success!!!")
+function init() {
+  console.log("loaded")
+  const myDiv = document.querySelector("#map")
+
+const location = {center: {
+    lat: 55.9468841,
+    lng: -3.2013412999999673
+    },
+    zoom: 18
+}
+new google.maps.Map(myDiv, location)
+}
+
