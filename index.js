6af819ba23b9e8b26f7787401443c4a33a95aadd
elipinska <ewa.lipinska@hotmail.com>
2017-10-09 19:58:50 +0100

    Add marker

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function init() {
   console.log("loaded")
   const myDiv = document.querySelector("#map")
+  
+const myLatLng = {lat: 55.9468841, lng: -3.2013412999999673}
 
-const location = {center: {
-    lat: 55.9468841,
-    lng: -3.2013412999999673
-    },
+const location = {center: myLatLng,
     zoom: 18
 }
-new google.maps.Map(myDiv, location)
+
+const map = new google.maps.Map(myDiv, location)
+
+const marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'This is where we learned JavaScript!'
+        });
+
+
 }
 
