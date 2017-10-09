9540cdb47f810e6305fd27b8b7db602078122b19
elipinska <ewa.lipinska@hotmail.com>
2017-10-09 20:11:11 +0100

    Add info box

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ const location = {center: myLatLng,
 
 const map = new google.maps.Map(myDiv, location)
 
+ const contentString = '<div id="content">'+ '<p>Come here to play with code!</p>'
++
+            '</div>';
+
+        const infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
 const marker = new google.maps.Marker({
           position: myLatLng,
           map: map,
           title: 'This is where we learned JavaScript!'
         });
 
+		marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
 
 }
 
